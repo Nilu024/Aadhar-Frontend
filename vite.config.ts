6afdc19cb1633056eaa94748c402d31bb4810ba0
Nilu024 <nileshdesale24@gmail.com>
2025-09-24 23:07:44 +0530

    Notificationn

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+  base: "/aadhar_app/", 
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
