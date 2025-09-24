9c669febb184df14e0c581d726a9e57d2f6fd14c
Nilu024 <nileshdesale24@gmail.com>
2025-09-24 23:12:08 +0530

    reverse

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/aadhar_app/", 
-  plugins: [react(), tailwindcss()],
+    plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
